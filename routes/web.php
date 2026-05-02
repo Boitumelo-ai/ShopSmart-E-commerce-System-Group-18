7f1053b9c16456402b3de8e2849e402140b8e32d
gmabe02 <gmabe02@gmail.com>
2026-05-02 21:31:22 +0200

    Change routes to singular form for roles, users, products

--- routes/web.php
+++ routes/web.php
@@ -23,29 +23,29 @@ Route::get('/', function () {
 // -----------------------------------------------------------------------
 // ROLE ROUTES
 // -----------------------------------------------------------------------
-Route::get('/roles', [RoleController::class, 'index']);
-Route::get('/roles/{id}', [RoleController::class, 'show']);
-Route::post('/roles', [RoleController::class, 'store']);
-Route::put('/roles/{id}', [RoleController::class, 'update']);
-Route::delete('/roles/{id}', [RoleController::class, 'destroy']);
+Route::get('/role', [RoleController::class, 'index']);
+Route::get('/role/{id}', [RoleController::class, 'show']);
+Route::post('/role', [RoleController::class, 'store']);
+Route::put('/role/{id}', [RoleController::class, 'update']);
+Route::delete('/role/{id}', [RoleController::class, 'destroy']);
 
 // -----------------------------------------------------------------------
 // USER ROUTES
 // -----------------------------------------------------------------------
-Route::get('/users', [UserController::class, 'index']);
-Route::get('/users/{id}', [UserController::class, 'show']);
-Route::post('/users', [UserController::class, 'store']);
-Route::put('/users/{id}', [UserController::class, 'update']);
-Route::delete('/users/{id}', [UserController::class, 'destroy']);
+Route::get('/user', [UserController::class, 'index']);
+Route::get('/user/{id}', [UserController::class, 'show']);
+Route::post('/user', [UserController::class, 'store']);
+Route::put('/user/{id}', [UserController::class, 'update']);
+Route::delete('/user/{id}', [UserController::class, 'destroy']);
 
 // -----------------------------------------------------------------------
 // PRODUCT ROUTES
 // -----------------------------------------------------------------------
-Route::get('/products', [ProductController::class, 'index']);
-Route::get('/products/{id}', [ProductController::class, 'show']);
-Route::post('/products', [ProductController::class, 'store']);
-Route::put('/products/{id}', [ProductController::class, 'update']);
-Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+Route::get('/product', [ProductController::class, 'index']);
+Route::get('/product/{id}', [ProductController::class, 'show']);
+Route::post('/product', [ProductController::class, 'store']);
+Route::put('/product/{id}', [ProductController::class, 'update']);
+Route::delete('/product/{id}', [ProductController::class, 'destroy']);
 
 // -----------------------------------------------------------------------
 // ORDER (USER GOODS) ROUTES
@@ -59,15 +59,15 @@ Route::delete('/usergoods/{id}', [UserGoodsController::class, 'destroy']);
 // -----------------------------------------------------------------------
 // PAYMENT ROUTES
 // -----------------------------------------------------------------------
-Route::get('/payments', [PaymentController::class, 'index']);
-Route::get('/payments/{id}', [PaymentController::class, 'show']);
-Route::post('/payments', [PaymentController::class, 'store']);
-Route::delete('/payments/{id}', [PaymentController::class, 'destroy']);
+Route::get('/payment', [PaymentController::class, 'index']);
+Route::get('/payment/{id}', [PaymentController::class, 'show']);
+Route::post('/payment', [PaymentController::class, 'store']);
+Route::delete('/payment/{id}', [PaymentController::class, 'destroy']);
 
 // -----------------------------------------------------------------------
 // REVIEW ROUTES
 // -----------------------------------------------------------------------
-Route::get('/reviews', [ReviewController::class, 'index']);
-Route::get('/reviews/{id}', [ReviewController::class, 'show']);
-Route::post('/reviews', [ReviewController::class, 'store']);
-Route::delete('/reviews/{id}', [ReviewController::class, 'destroy']);
+Route::get('/review', [ReviewController::class, 'index']);
+Route::get('/review/{id}', [ReviewController::class, 'show']);
+Route::post('/review', [ReviewController::class, 'store']);
+Route::delete('/review/{id}', [ReviewController::class, 'destroy']);
